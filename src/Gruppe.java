44ee0099058cb8452b3bdd6aa33d4fba7c5c1668
Adrian Steffenakk <adrste10@hotmail.com>
2017-03-16 14:51:56 +0100

    Made gruppe-object

--- src/Gruppe.java
+++ src/Gruppe.java
@@ -1,3 +1,4 @@
+import javax.swing.plaf.nimbus.State;
 import java.sql.*;
 import java.util.Scanner;
 
@@ -25,12 +26,64 @@ public class Gruppe {
     public int getSubGruppeId() { return subGruppeId; }
     public int getØvelseId() { return øvelseId; }
 
-    public void makeGruppe(Scanner scanner){
+
+    public void makeGruppe(Scanner scanner) throws SQLException {
+        System.out.println("Lag en ny treningsgruppe");
+
         System.out.println("Hva vil du kalle gruppen?");
         kategori = scanner.nextLine();
         System.out.println("Gi en beskrivelse av øvelsene i gruppen: (kan være tom)");
         beskrivelse = scanner.nextLine();
+        System.out.println("Hvilken øvelse vil du legge i gruppen?");
+        getAlleØvelser(scanner);
 
+        String gruppeSql = String.format("INSERT INTO gruppe VALUES('%s', '%s', %d)", getKategori(), getBeskrivelse(), getØvelseId());
+
+        System.out.println("Er du sikker på at du vil legge til denne gruppen? (ja/nei)");
+        String godkjenn = scanner.nextLine();
+        if (godkjenn.equals("ja")){
+            Statement stmt = conn.createStatement();
+            stmt.executeUpdate(gruppeSql);
+            flereGrupper(scanner);
+        } else {
+            System.out.println("Avbrutt, ingen grupper ble lagt til i databasen.");
+        }
     }
+
+
+    private void flereGrupper(Scanner scanner) throws SQLException {
+        System.out.println("Vil du legge til flere grupper?");
+        String godkjenn = scanner.nextLine();
+        while (godkjenn.equals("ja")){
+            makeGruppe(scanner);
+        }
+        System.out.println("Avbrutt, ingen flere grupper.");
+        return;
+    }
+
+
+    public void getAlleØvelser(Scanner scanner) throws SQLException {
+        System.out.println("Velg deg en øvelse");
+        String query = "SELECT øvelseId, navn FROM øvelse";
+        try {
+            ResultSet rs = getResultSet(conn, query);
+            while (rs.next()){
+                System.out.println(rs.getInt("øvelseId") + ", " + rs.getString("navn"));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Skriv inn ID til øvelsen din");
+        øvelseId = Integer.parseInt(scanner.nextLine());
+    }
+
+
+    private static ResultSet getResultSet(Connection conn, String query) throws SQLException {
+        Statement stmt = conn.createStatement();
+        return stmt.executeQuery(query);
+    }
+
+
+
 
 }
